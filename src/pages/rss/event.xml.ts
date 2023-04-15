dc7a0adbf78a88f50ab076f27ee4b5a4a7be18b0
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-04-15 13:29:45 +0900

    feat: RSSでコンテンツHTMLも配信できるようにした(#5)
    ref: https://docs.astro.build/en/guides/rss/#including-full-post-content

--- src/pages/rss/event.xml.ts
+++ src/pages/rss/event.xml.ts
@@ -1,7 +1,11 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
 import { SITE_TITLE, SITE_DESCRIPTION } from "@/consts";
 import { extractPostSlug } from "@/utils/extractPostSlug";
+
+const parser = new MarkdownIt();
 
 export async function get(context) {
   const posts = (await getCollection("posts"))
@@ -18,6 +22,7 @@ export async function get(context) {
     items: posts.map((post) => ({
       ...post.data,
       link: `/posts/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
     })),
     stylesheet: "/pretty-feed-v3.xsl",
   });
