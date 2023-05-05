74575193037a8a0a38a84a7bce4deb5da78314d5
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-05-05 18:58:31 +0900

    settings: shikiのテーマをdark+にした(#15)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import UnoCSS from "unocss/astro";
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://changeofpace.site",
+  site: 'https://changeofpace.site',
   integrations: [mdx(), sitemap(), UnoCSS()],
   build: {
-    format: "file",
+    format: 'file',
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'dark-plus',
+    },
   },
 });
