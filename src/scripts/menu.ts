5c45ed4024bacf93e20b33863c41f88124ff5088
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-05-14 16:17:10 +0900

    feat: Menuコンポーネントでbackdropも含めるようにした(#31)

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -2,11 +2,17 @@
 const openMenuButton = document.getElementById('open-menu-button');
 const menu = document.getElementById('menu');
 const baseContents = document.getElementById('base-contents');
+const menuBackDrop = document.getElementById('menu-backdrop');
 const closeMenuButton = document.getElementById('close-menu-button');
 
 openMenuButton?.addEventListener('click', () => {
   menu?.classList.remove('hidden');
   baseContents?.setAttribute('inert', '');
+});
+
+menuBackDrop?.addEventListener('click', () => {
+  menu?.classList.add('hidden');
+  baseContents?.removeAttribute('inert');
 });
 
 closeMenuButton?.addEventListener('click', () => {
