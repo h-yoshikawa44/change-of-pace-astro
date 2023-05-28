849e0d11c88508cec35cba10823e40483ff7b68d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-05-28 19:31:50 +0900

    feat: メニューのスクロール制御対応(#43)
    背景はスクロールできないように。
    メニューはスクロールできるように。

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,7 +1,13 @@
 /* eslint-env browser */
 import { MENU_TYPE_LIST } from '@/consts';
 
+const body = document.body;
 const baseContents = document.getElementById('base-contents');
+/**
+ * スクロールバーを非表示にすると、その分右にずれるため padding で調整するスタイル
+ * body 要素に Tailwind のクラス付与ができなかったので setAttribute で style に付与して使う
+ */
+const scrollHiddenClass = 'overflow:hidden; padding-right: 17px';
 
 MENU_TYPE_LIST.forEach((type) => {
   const openMenuButton = document.getElementById(`open-${type}-menu-button`);
@@ -12,15 +18,18 @@ MENU_TYPE_LIST.forEach((type) => {
   openMenuButton?.addEventListener('click', () => {
     menu?.classList.remove('hidden');
     baseContents?.setAttribute('inert', '');
+    body?.setAttribute('style', scrollHiddenClass);
   });
 
   menuBackDrop?.addEventListener('click', () => {
     menu?.classList.add('hidden');
     baseContents?.removeAttribute('inert');
+    body.removeAttribute('style');
   });
 
   closeMenuButton?.addEventListener('click', () => {
     menu?.classList.add('hidden');
     baseContents?.removeAttribute('inert');
+    body.removeAttribute('style');
   });
 });
