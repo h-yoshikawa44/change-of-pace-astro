a858dc344b83f9cf62673377b7b5a69b89d3e966
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-05-28 17:49:50 +0900

    feat: カスタム下線スタイル作成、サイドバー切り替えボタンに適用(#43)

--- src/scripts/sideBar.ts
+++ src/scripts/sideBar.ts
@@ -7,10 +7,14 @@ const sideBarAnchorList = document.getElementById('sidebar-anchor-list');
 
 selectMenuButton?.addEventListener('click', () => {
   sideBarMenu?.classList.remove('hidden');
+  selectMenuButton?.classList.add('custom-after-border-b');
   sideBarAnchorList?.classList.add('hidden');
+  selectAnchorButton?.classList.remove('custom-after-border-b');
 });
 
 selectAnchorButton?.addEventListener('click', () => {
   sideBarAnchorList?.classList.remove('hidden');
+  selectAnchorButton?.classList.add('custom-after-border-b');
   sideBarMenu?.classList.add('hidden');
+  selectMenuButton?.classList.remove('custom-after-border-b');
 });
