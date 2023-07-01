6ff9e82c33cc715f31acf1cdee572676dc49ab5d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-07-01 19:36:34 +0900

    feat: APIルートでads.txtの配置(#23)

---
+++ src/pages/ads.txt.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from 'astro';
+
+const GOOGLE_ADSENSE_PUBLISHER_ID =
+  import.meta.env.GOOGLE_ADSENSE_PUBLISHER_ID.replace('ca-', '');
+
+export const get: APIRoute = async () => {
+  return {
+    body: `google.com, ${GOOGLE_ADSENSE_PUBLISHER_ID}, DIRECT, f08c47fec0942fa0`,
+  };
+};
