dac9230d1c150f8a523648c1f6e5c3ac05288a8b
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-09-09 18:20:40 +0900

    feat: ads.txtのエンドポイント定義を新方式に移行(#102)

--- src/pages/ads.txt.ts
+++ src/pages/ads.txt.ts
@@ -1,10 +1,11 @@
+/* global Response */
 import type { APIRoute } from 'astro';
 
 const GOOGLE_ADSENSE_PUBLISHER_ID =
   import.meta.env.PUBLIC_GOOGLE_ADSENSE_PUBLISHER_ID.replace('ca-', '');
 
-export const get: APIRoute = async () => {
-  return {
-    body: `google.com, ${GOOGLE_ADSENSE_PUBLISHER_ID}, DIRECT, f08c47fec0942fa0`,
-  };
+export const GET: APIRoute = async () => {
+  return new Response(
+    `google.com, ${GOOGLE_ADSENSE_PUBLISHER_ID}, DIRECT, f08c47fec0942fa0`
+  );
 };
