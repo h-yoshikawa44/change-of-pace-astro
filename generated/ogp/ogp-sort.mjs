978724111ab9755b5ad8f5896bdfb67c55d30d64
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-11-24 20:02:45 +0900

    feat: OGPキャッシュデータを差分がわかりやすいようにsortするスクリプト作成 + sort(#136)

---
+++ generated/ogp/ogp-sort.mjs
@@ -0,0 +1,46 @@
+// OGP キャッシュデータを差分がわかりやすいように sort するスクリプト
+// ファイル名: 降順 > リンク URL: 昇順
+
+import { writeFileSync, readFileSync } from 'node:fs';
+
+const postYearList = ['2018', '2019', '2020', '2021', '2022', '2023'];
+
+/**
+ * オブジェクトの中身をキーをもとに並び替えしたものを返す
+ * @param {any} obj オブジェクト
+ * @param {'asc' | 'desc'} sortType キーの並び替え順
+ */
+const sortObjectFromKey = (obj, sortType = 'asc') => {
+  let keyList = Object.keys(obj).sort();
+
+  if (sortType === 'desc') {
+    keyList = keyList.reverse();
+  }
+
+  return keyList.reduce((prev, current) => {
+    prev[current] = {
+      ...prev[current],
+      ...obj[current],
+    };
+
+    return prev;
+  }, {});
+};
+
+for (const year of postYearList) {
+  const generatedFilePath = `./generated/ogp/${year}.json`;
+
+  const ogp = JSON.parse(readFileSync(generatedFilePath, 'utf8'));
+  const fileNameList = Object.keys(ogp).sort().reverse();
+  const sortOgp = fileNameList.reduce((prev, current) => {
+    const ogUrlObj = sortObjectFromKey(ogp[current]);
+
+    prev[current] = {
+      ...prev[current],
+      ...ogUrlObj,
+    };
+    return prev;
+  }, {});
+
+  writeFileSync(generatedFilePath, JSON.stringify(sortOgp, null, 2));
+}
