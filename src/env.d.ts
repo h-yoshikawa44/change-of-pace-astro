cdc4a3780fc6448807fa4345706a72d41160585d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-09-08 19:50:17 +0900

    fix: env.d.tsを最新ベースに更新(#183)
    Astro 4.14.0での推奨設定。

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars, no-redeclare */
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
 
 interface ImportMetaEnv {
   readonly GOOGLE_ANALYTICS_TAG: string;
